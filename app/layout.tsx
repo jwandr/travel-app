f91d735bd72f49eab96791f1d4843f9075fec24b
Joss Gray <jossgray@icloud.com>
2026-05-04 18:29:25 +0800

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+<link rel="icon" href="/favicon.png" type="image" />
 
 const inter = Inter({ subsets: ['latin'] })
 
